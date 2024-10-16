4ea0ab44cf73fe102dc917a05c99d3a2ba5082f8
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-10-17 00:02:41 +0600

    Create TowerOfHanoi_4.cs

---
+++ TowerOfHanoi_4/TowerOfHanoi_4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TowerOfHanoi_4
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            int n = 3;
+            String from = "A";
+            String to = "B";
+            String via = "C";
+            hanoi(n, from, via, to);
+        }
+        public static void hanoi(int n, String from, String to, String via)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine("Move disk 1 from " + from + " to " + to);
+            }
+            else
+            {
+                hanoi(n - 1, from, via, to);
+                Console.WriteLine("Move disk " + n + " from " + from + " to " + to);
+                hanoi(n - 1, via, to, from);
+            }
+        }
+
+
+    }
+}
