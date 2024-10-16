23a0222c3ab6dbb08375350b819d985665872d22
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-10-17 00:01:37 +0600

    Create  FibonacciSeries_3.cs

---
+++ FibonacciSeries_3/FibonacciSeries_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FibonacciSeries_3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.WriteLine("Please enter a number : ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");    
+            for (i = 2; i < number; ++i) 
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.WriteLine();
+        }
+    }
+}
