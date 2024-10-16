4398f9e0a8eec98a3846e39aae16fb50e0bd001d
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-10-17 00:03:32 +0600

    Create JaggedArray__5.cs

---
+++ JaggedArray _5/JaggedArray _5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace JaggedArray__5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[][,] jaggedArray = new int[3][,]  {
+            new int[ , ] { {1, 8}, {6, 7} },
+            new int[ , ] { {0, 3}, {5, 6}, {9, 10} },
+            new int[ , ] { {11, 23}, {100, 88}, {0, 10} }
+        };
+        
+            Console.WriteLine("From jagged Array, taking specific elements : ");
+            Console.WriteLine(jaggedArray[0][0, 1]);
+            Console.WriteLine(jaggedArray[1][2, 1]);
+            Console.WriteLine(jaggedArray[2][1, 0]);
+            Console.ReadLine();
+        }
+    }
+}
